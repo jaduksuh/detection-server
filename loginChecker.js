b9caf1624596abae6d5cb2df70e6e53332805b48
jaduksuh <jaduksuh@plusidentity.com>
2021-08-20 18:07:56 +0900

    Fixed code for heroku deployment

--- loginChecker.js
+++ loginChecker.js
@@ -15,119 +15,121 @@ let mailTransporter = nodemailer.createTransport({
   }
 });
 
-(async () => {
-  for(let website in websites) {
-    const browser = await puppeteer.launch({ 
-      headless: false,
-      args: ['--no-sandbox'] 
-    });
-    const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
-    try { 
-      await page.goto(websites[website]["url"]);
-      const title = await page.title();
-      const url = await page.url();
-      console.log(title, url);
+cron.schedule('0,10,20,30,40,50 * * * *', () => {
+  (async () => {
+    for(let website in websites) {
+      const browser = await puppeteer.launch({ 
+        headless: false,
+        args: ['--no-sandbox'] 
+      });
+      const page = await browser.newPage();
+      await page.setDefaultNavigationTimeout(0);
+      try { 
+        await page.goto(websites[website]["url"]);
+        const title = await page.title();
+        const url = await page.url();
+        console.log(title, url);
 
-      if("startButton" in websites[website]) {
-        for (button of websites[website]["startButton"]) {
-          await page.click(button);
-          await new Promise(resolve => setTimeout(resolve, 500));
+        if("startButton" in websites[website]) {
+          for (button of websites[website]["startButton"]) {
+            await page.click(button);
+            await new Promise(resolve => setTimeout(resolve, 500));
+          }
+          await new Promise(resolve => setTimeout(resolve, 1000));
         }
-        await new Promise(resolve => setTimeout(resolve, 1000));
-      }
 
-      // wait 2.5 seconds for the page to fully load
-      await new Promise(resolve => setTimeout(resolve, 2500));
-      
-      const findSelector = (element) => {
-        let webfile = require(`./config_files/${website}.js`);
-        let cssElements = [];
-        switch (element) {
-          case "email":
-            for (let field of webfile["manualActionConfig"]["login"]["listenFields"]) {
-              if (field["content"]["dataField"] === "_emailAddress") {
-                cssElements = cssElements.concat(field["content"]["elementSelectors"]);
-              }
-            }
-            return cssElements;
-            break;
-          case "password":
-            for (let field of webfile["manualActionConfig"]["login"]["listenFields"]) {
-              if (field["content"]["dataField"] === "password") {
-                cssElements = cssElements.concat(field["content"]["elementSelectors"]);
-              }
-            }
-            return cssElements;
-            break;
-          case "username":
-            for (let field of webfile["manualActionConfig"]["login"]["listenFields"]) {
-              if (field["content"]["dataField"] === "_username") {
-                cssElements = cssElements.concat(field["content"]["elementSelectors"]);
-              }
-            }
-            return cssElements;
-            break;
-          case "button":
-            for (let field of webfile["manualActionConfig"]["login"]["triggerEvents"]) {
-              cssElements = cssElements.concat(field["content"]["elementSelectors"]);
-            }
-            return cssElements;
-            break;
-          default:
-            throw new Error("Unidentified element type!!");
-        }
-      }
-
-      for(let element of websites[website]["elements"]) {
-        let targetElements = findSelector(element); console.log(targetElements);
-        let elementExists = false;
+        // wait 2.5 seconds for the page to fully load
+        await new Promise(resolve => setTimeout(resolve, 2500));
         
-        for (targetElement of targetElements) {
-          const grabElement = await page.evaluate((targetElement) => {
-            return document.querySelector(targetElement);      
-          }, targetElement);
-          if(grabElement !== null) {
-            elementExists = true; break;
+        const findSelector = (element) => {
+          let webfile = require(`./config_files/${website}.js`);
+          let cssElements = [];
+          switch (element) {
+            case "email":
+              for (let field of webfile["manualActionConfig"]["login"]["listenFields"]) {
+                if (field["content"]["dataField"] === "_emailAddress") {
+                  cssElements = cssElements.concat(field["content"]["elementSelectors"]);
+                }
+              }
+              return cssElements;
+              break;
+            case "password":
+              for (let field of webfile["manualActionConfig"]["login"]["listenFields"]) {
+                if (field["content"]["dataField"] === "password") {
+                  cssElements = cssElements.concat(field["content"]["elementSelectors"]);
+                }
+              }
+              return cssElements;
+              break;
+            case "username":
+              for (let field of webfile["manualActionConfig"]["login"]["listenFields"]) {
+                if (field["content"]["dataField"] === "_username") {
+                  cssElements = cssElements.concat(field["content"]["elementSelectors"]);
+                }
+              }
+              return cssElements;
+              break;
+            case "button":
+              for (let field of webfile["manualActionConfig"]["login"]["triggerEvents"]) {
+                cssElements = cssElements.concat(field["content"]["elementSelectors"]);
+              }
+              return cssElements;
+              break;
+            default:
+              throw new Error("Unidentified element type!!");
           }
         }
 
-        if(elementExists) console.log("Existing Element!");
-        else {
-          console.log("Non-existent element!");
-          let mailDetails = {
-            from: 'oprah.plusidentity@gmail.com',
-            to: 'jaduksuh@plusidentity.com',
-            subject: `WARNING: Change of login process detected for ${website}!!`,
-            text: `The Plusidentity detection-server just detected a change of login process for ${website}.\nPlease resolve the issue ASAP.`
-          };
-          mailTransporter.sendMail(mailDetails, (err, data) => {
-            if(err) {
-              console.log('Error occurred while sending email.');
-            } else {
-              console.log('Error email sent successfully.');
+        for(let element of websites[website]["elements"]) {
+          let targetElements = findSelector(element); console.log(targetElements);
+          let elementExists = false;
+          
+          for (targetElement of targetElements) {
+            const grabElement = await page.evaluate((targetElement) => {
+              return document.querySelector(targetElement);      
+            }, targetElement);
+            if(grabElement !== null) {
+              elementExists = true; break;
             }
-          });
-          break;
+          }
+
+          if(elementExists) console.log("Existing Element!");
+          else {
+            console.log("Non-existent element!");
+            let mailDetails = {
+              from: 'oprah.plusidentity@gmail.com',
+              to: 'jaduksuh@plusidentity.com',
+              subject: `WARNING: Change of login process detected for ${website}!!`,
+              text: `The Plusidentity detection-server just detected a change of login process for ${website}.\nPlease resolve the issue ASAP.`
+            };
+            mailTransporter.sendMail(mailDetails, (err, data) => {
+              if(err) {
+                console.log('Error occurred while sending email.');
+              } else {
+                console.log('Error email sent successfully.');
+              }
+            });
+            break;
+          }
         }
       }
+      catch (e) {
+        console.log(e);
+        let mailDetails = {
+          from: 'oprah.plusidentity@gmail.com',
+          to: 'jaduksuh@plusidentity.com',
+          subject: `ERROR: Login process error for ${website}!!`,
+          text: `The Plusidentity detection-server just detected an error in the login process for ${website}.\nPlease resolve the issue.`
+        };
+        mailTransporter.sendMail(mailDetails, (err, data) => {
+          if(err) {
+            console.log('Error occurred while sending email.');
+          } else {
+            console.log('Error email sent successfully.');
+          }
+        });
+      }
+      await browser.close();
     }
-    catch (e) {
-      console.log(e);
-      let mailDetails = {
-        from: 'oprah.plusidentity@gmail.com',
-        to: 'jaduksuh@plusidentity.com',
-        subject: `ERROR: Login process error for ${website}!!`,
-        text: `The Plusidentity detection-server just detected an error in the login process for ${website}.\nPlease resolve the issue.`
-      };
-      mailTransporter.sendMail(mailDetails, (err, data) => {
-        if(err) {
-          console.log('Error occurred while sending email.');
-        } else {
-          console.log('Error email sent successfully.');
-        }
-      });
-    }
-    await browser.close();
-  }
-})();
+  })();
+});
