bf6866c1ab4ae3b8957d7c1ecaae8e8825465ecc
jaduksuh <jaduksuh@plusidentity.com>
2021-08-24 15:34:51 +0900

    Added express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const cron = require('node-cron');
 const loginChecker = require('./loginChecker');
 const signupChecker = require('./signupChecker');
@@ -5,4 +6,7 @@ const signupChecker = require('./signupChecker');
 cron.schedule('0,5,10,15,20,25,30,35,40,45,50,55 * * * *', () => {
   loginChecker.loginCheck();
 	signupChecker.signupCheck();
-})
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
