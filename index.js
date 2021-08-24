b912ecf48f7e6e2c4d731891b750b59d63f47ffd
jaduksuh <jaduksuh@plusidentity.com>
2021-08-24 16:12:10 +0900

    Changed time interval back to 30 minutes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 const loginChecker = require('./loginChecker');
 const signupChecker = require('./signupChecker');
 
-cron.schedule('0,5,10,15,20,25,30,35,40,45,50,55 * * * *', () => {
+cron.schedule('0,30 * * * *', () => {
   loginChecker.loginCheck();
 	signupChecker.signupCheck();
 });
