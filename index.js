fee7674f457036e307b1ca3cba6cd8d98e7afec4
jaduksuh <jaduksuh@plusidentity.com>
2021-08-24 15:07:14 +0900

    Fix time interval to 5 minutes (for testing)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const loginChecker = require('./loginChecker');
 const signupChecker = require('./signupChecker');
 
-cron.schedule('0,30 * * * *', () => {
+cron.schedule('0,5,10,15,20,25,30,35,40,45,50,55 * * * *', () => {
   loginChecker.loginCheck();
 	signupChecker.signupCheck();
 })
