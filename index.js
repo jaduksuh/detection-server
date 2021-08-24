9198572af4cceb78d2435d58db489d6da089b4d9
jaduksuh <jaduksuh@plusidentity.com>
2021-08-24 16:54:18 +0900

    Changed duration to 20 minutes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 const loginChecker = require('./loginChecker');
 const signupChecker = require('./signupChecker');
 
-cron.schedule('0,30 * * * *', () => {
+cron.schedule('0,20,40 * * * *', () => {
   loginChecker.loginCheck();
 	signupChecker.signupCheck();
 });
